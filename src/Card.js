27384bd5ab5bdb307fa2014cf221359851a5648d
Stella David <sdavi585@mtroyal.ca>
2024-07-31 00:29:45 -0600

    Imported tachyons and added user javascript file array of object (different types of users)

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Card = () => {
+    return (
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow dw2 shadow-5">
+            <img src="https://robohash.org/bc"></img>
+            <div>
+                <h2>Person 1</h2>
+                <p>Person1 Email</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
   </React.StrictMode>
 );
 
