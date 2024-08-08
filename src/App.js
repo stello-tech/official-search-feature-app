7af0732183005b1c5a3dcf966639533a91643da1
Stella David <sdavi585@mtroyal.ca>
2024-08-08 09:04:39 -0600

    Commenting SearchBox and App for context

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ class App extends Component {
     }
 
     render() {
-		const filterUsers = this.state.users.filter(users =>{
-		return users.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-		})
+                const filterUsers = this.state.users.filter (users => {
+                return users.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+                })
 		return (
 			<div className ='tc'>
 				<h1 className="f1"> First React App </h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<SearchBox searchChange={this.onSearchChange}/> 
 				<CardList users={filterUsers} />
 			</div>
 		);
@@ -33,3 +33,12 @@ class App extends Component {
 	
 }
 export default App;
+
+// We're calling the searchBox component through line #28, in which we pass the property 
+// searchComponent. The searchChange property calls the function onSearchChange by {this.onSearchChange}.
+
+// The onSearchChange function is refering to line #16 which is giving value to the searchField in line #12.
+// The value being the user's input.
+
+// In the rendering function, we are creating another variable filterUsers which recieves the users in line
+// #11 (arrOfObj from User.js) and since this.state.users is an array we are using the filter function.
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -12,3 +12,7 @@ const SearchBox = ({searchChange}) => {
 
 }
 export default SearchBox;
+
+// We make a component called SearchBox that has a div containing an input which is the "search" type. 
+// This input event calls on the HTML event thats the onChange event that calls on the prop 
+// (property = searchChange). This property is passed to line #28 in App.js.
